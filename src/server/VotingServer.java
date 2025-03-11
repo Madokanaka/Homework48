f2162f52c0361ad3868f8917fe739e822ca07ab4
Seitek <seita20078@gmail.com>
2025-03-11 11:24:08 +0600

    Added handling of login

--- src/server/VotingServer.java
+++ src/server/VotingServer.java
@@ -6,6 +6,7 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 import model.Candidate;
+import model.User;
 import util.JsonUtil;
 import util.Utils;
 
@@ -13,23 +14,34 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 
-public class VotingServer extends BasicServer{
+public class VotingServer extends BasicServer {
     private final static Configuration freemarker = initFreeMarker();
+    protected final Map<String, User> sessions = new ConcurrentHashMap<>();
 
     public VotingServer(String host, int port) throws IOException {
         super(host, port);
         registerGet("/", this::handleCandidatesPage);
-        registerGet("/vote", this::handleCandidatesVote);
+        registerPost("/vote", this::handleCandidatesVote);
         registerGet("/error", this::handleCandidateError);
         registerGet("/thank-you", this::handleThankYouPage);
         registerGet("/votes", this::handleVotesPage);
+        registerGet("/login", this::loginGet);
+        registerPost("/login", this::loginPost);
+        registerGet("/login-error", this::loginError);
+        registerGet("/register", this::handleRegisterPage);
+        registerPost("/register", this::handleRegisterPost);
+        registerGet("/register-error", this::registerErrorPage);
+        registerGet("/register-success", this::registerSuccessPage);
     }
 
 
@@ -70,72 +82,114 @@ public class VotingServer extends BasicServer{
     }
 
     private void handleCandidatesPage(HttpExchange exchange) {
-        Map<String, Object> dataModel = new HashMap<>();
-        dataModel.put("candidates", JsonUtil.getCandidates());
-        renderTemplate(exchange, "/candidates.ftlh", dataModel);
+        String cookieString = getCookie(exchange);
+        Map<String, String> cookies = Cookie.parse(cookieString);
+        String sessionId = cookies.get("sessionId");
+
+        User loggedInUser = sessionId != null ? sessions.get(sessionId) : null;
+
+        if (loggedInUser != null) {
+            Map<String, Object> dataModel = new HashMap<>();
+            dataModel.put("candidates", JsonUtil.getCandidates());
+            renderTemplate(exchange, "/candidates.ftlh", dataModel);
+        } else {
+            renderTemplate(exchange, "register-login/unlogged.html", Collections.emptyMap());
+        }
+
     }
 
     private void handleCandidatesVote(HttpExchange exchange) {
-        String queryParams = getQueryParams(exchange);
-        Map<String, String> params = Utils.parseUrlEncoded(queryParams, "&");
-        String idStr = params.get("id");
+        String cookieString = getCookie(exchange);
+        Map<String, String> cookies = Cookie.parse(cookieString);
+        String sessionId = cookies.get("sessionId");
 
-        if (idStr == null) {
-            redirect303(exchange, "/error");
-            return;
+        User loggedInUser = sessionId != null ? sessions.get(sessionId) : null;
+
+        if (loggedInUser != null) {
+            String queryParams = getBody(exchange);
+            Map<String, String> params = Utils.parseUrlEncoded(queryParams, "&");
+            String idStr = params.get("id");
+
+            if (idStr == null) {
+                redirect303(exchange, "/error");
+                return;
+            }
+
+            int id;
+            try {
+                id = Integer.parseInt(idStr);
+            } catch (NumberFormatException e) {
+                redirect303(exchange, "/error");
+                return;
+            }
+
+            Candidate candidate = JsonUtil.findCandidateById(id);
+            if (candidate == null) {
+                redirect303(exchange, "/error");
+                return;
+            }
+
+            JsonUtil.voteForCandidate(id);
+            redirect303(exchange, "/thank-you?id=" + id);
+        } else {
+            renderTemplate(exchange, "register-login/unlogged.html", Collections.emptyMap());
         }
 
-        int id;
-        try {
-            id = Integer.parseInt(idStr);
-        } catch (NumberFormatException e) {
-            redirect303(exchange, "/error");
-            return;
-        }
-
-        Candidate candidate = JsonUtil.findCandidateById(id);
-        if (candidate == null) {
-            redirect303(exchange, "/error");
-            return;
-        }
-
-        JsonUtil.voteForCandidate(id);
-        redirect303(exchange, "/thank-you?id=" + id);
     }
 
 
     private void handleCandidateError(HttpExchange exchange) {
-        renderTemplate(exchange, "error.html", null);
+        String cookieString = getCookie(exchange);
+        Map<String, String> cookies = Cookie.parse(cookieString);
+        String sessionId = cookies.get("sessionId");
+
+        User loggedInUser = sessionId != null ? sessions.get(sessionId) : null;
+
+        if (loggedInUser != null) {
+            renderTemplate(exchange, "error.html", null);
+        } else {
+            renderTemplate(exchange, "register-login/unlogged.html", Collections.emptyMap());
+        }
     }
 
     private void handleThankYouPage(HttpExchange exchange) {
-        String queryParams = getQueryParams(exchange);
-        Map<String, String> params = Utils.parseUrlEncoded(queryParams, "&");
-        String idStr = params.get("id");
+        String cookieString = getCookie(exchange);
+        Map<String, String> cookies = Cookie.parse(cookieString);
+        String sessionId = cookies.get("sessionId");
 
-        if (idStr == null) {
-            redirect303(exchange, "/error");
-            return;
+        User loggedInUser = sessionId != null ? sessions.get(sessionId) : null;
+
+        if (loggedInUser != null) {
+            String queryParams = getQueryParams(exchange);
+            Map<String, String> params = Utils.parseUrlEncoded(queryParams, "&");
+            String idStr = params.get("id");
+
+            if (idStr == null) {
+                redirect303(exchange, "/error");
+                return;
+            }
+
+            int id;
+            try {
+                id = Integer.parseInt(idStr);
+            } catch (NumberFormatException e) {
+                redirect303(exchange, "/error");
+                return;
+            }
+
+            Candidate candidate = JsonUtil.findCandidateById(id);
+            if (candidate == null) {
+                redirect303(exchange, "/error");
+                return;
+            }
+
+            Map<String, Object> dataModel = new HashMap<>();
+            dataModel.put("candidate", candidate);
+            dataModel.put("totalVotes", JsonUtil.getTotalVotes());
+            renderTemplate(exchange, "/thankyou.ftlh", dataModel);
+        } else {
+            renderTemplate(exchange, "register-login/unlogged.html", Collections.emptyMap());
         }
-
-        int id;
-        try {
-            id = Integer.parseInt(idStr);
-        } catch (NumberFormatException e) {
-            redirect303(exchange, "/error");
-            return;
-        }
-
-        Candidate candidate = JsonUtil.findCandidateById(id);
-        if (candidate == null) {
-            redirect303(exchange, "/error");
-            return;
-        }
-
-        Map<String, Object> dataModel = new HashMap<>();
-        dataModel.put("candidate", candidate);
-        dataModel.put("totalVotes", JsonUtil.getTotalVotes());
-        renderTemplate(exchange, "/thankyou.ftlh", dataModel);
     }
 
     private void handleVotesPage(HttpExchange exchange) {
@@ -146,6 +200,88 @@ public class VotingServer extends BasicServer{
         dataModel.put("candidates", candidates);
         dataModel.put("totalVotes", JsonUtil.getTotalVotes());
         renderTemplate(exchange, "votes.ftlh", dataModel);
+    }
+
+    private void loginGet(HttpExchange exchange) {
+        Path path = makeFilePath("register-login/login.ftlh");
+        sendFile(exchange, path, ContentType.TEXT_HTML);
+    }
+
+    private void loginPost(HttpExchange exchange) {
+        String raw = getBody(exchange);
+        Map<String, String> params = Utils.parseUrlEncoded(raw, "&");
+        String email = params.get("email");
+        String password = params.get("user-password");
+
+        User user = JsonUtil.getUserByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            String sessionId = UUID.randomUUID().toString();
+            sessions.put(sessionId, user);
+
+            Cookie sessionCookie = Cookie.make("sessionId", sessionId);
+            sessionCookie.setMaxAge(600);
+            sessionCookie.setHttpOnly(true);
+            setCookie(exchange, sessionCookie);
+
+            redirect303(exchange, "/");
+        } else {
+            redirect303(exchange, "/login-error");
+        }
+    }
+
+    private void loginError(HttpExchange exchange) {
+        Path path = makeFilePath("register-login/login_error.ftlh");
+        sendFile(exchange, path, ContentType.TEXT_HTML);
+    }
+
+    private void handleRegisterPage(HttpExchange exchange) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("error", null);
+        renderTemplate(exchange, "register-login/register.ftlh", data);
+    }
+
+    private void handleRegisterPost(HttpExchange exchange) {
+
+        String raw = getBody(exchange);
+
+        Map<String, String> params = Utils.parseUrlEncoded(raw, "&");
+        String email = params.get("email");
+        String name = params.get("name");
+        String password = params.get("password");
+
+        if (email == null || email.isEmpty() ||
+                name == null || name.isEmpty() ||
+                password == null || password.isEmpty()) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("error", "Все поля должны быть заполнены!");
+            renderTemplate(exchange, "register-login/register.ftlh", data);
+            return;
+        }
+
+        if (JsonUtil.getUserByEmail(email) != null) {
+            redirect303(exchange, "/register-error");
+        } else {
+            User newUser = new User(name, email, password);
+            JsonUtil.addUser(newUser);
+
+            redirect303(exchange, "/register-success");
+        }
+    }
+
+    private void registerSuccessPage(HttpExchange exchange) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("success", true);
+        data.put("message", "Регистрация успешна! Теперь вы можете войти.");
+
+        renderTemplate(exchange, "register-login/register_result.ftlh", data);
+    }
+
+    private void registerErrorPage(HttpExchange exchange) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("success", false);
+        data.put("message", "Пользователь с таким email уже зарегистрирован!");
+
+        renderTemplate(exchange, "register-login/register_result.ftlh", data);
     }
 
 }
